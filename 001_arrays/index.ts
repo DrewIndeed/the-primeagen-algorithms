8ae9dde18334b7f8c6f4ccc83f03bb1846c4a230
an.le6 <truongan1730@gmail.com>
2022-09-26 16:44:15 +0700

    0. linear search

---
+++ 001_arrays/index.ts
@@ -0,0 +1,4 @@
+import linear_search from "./linear_search";
+
+console.log(linear_search([1,2,3,4], 4));
+console.log(linear_search([1,2,3,4], 12));
---
+++ 001_arrays/linear_search.ts
@@ -0,0 +1,6 @@
+export default function linear_search(haystack: number[], needle: number): number {
+    for (let i=0;i<haystack.length;++i) {
+        if (haystack[i] === needle) return i;
+    }
+    return -1;
+}
