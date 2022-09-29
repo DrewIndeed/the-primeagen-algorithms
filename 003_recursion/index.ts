e9ec8c97dbf06f88d2db277102a761c54ad06c0b
an.le6 <truongan1730@gmail.com>
2022-09-29 15:26:26 +0700

    7. recursion maze solver

---
+++ 003_recursion/index.ts
@@ -0,0 +1,12 @@
+import maze_solver from "./maze_solver";
+
+const maze = [
+    "xxxxxxxxxx x",
+    "x        x x",
+    "x        x x",
+    "x  xxxxxxx x",
+    "x          x",
+    "x xxxxxxxxxx",
+];
+
+console.log(maze_solver(maze, "x", { x: 10, y: 0 }, { x: 1, y: 5 }));
---
+++ 003_recursion/out_js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const maze_solver_1 = require("./maze_solver");
+const maze = [
+    "xxxxxxxxxx x",
+    "x        x x",
+    "x        x x",
+    "x  xxxxxxx x",
+    "x          x",
+    "x xxxxxxxxxx",
+];
+console.log((0, maze_solver_1.default)(maze, "x", { x: 10, y: 0 }, { x: 1, y: 5 }));
